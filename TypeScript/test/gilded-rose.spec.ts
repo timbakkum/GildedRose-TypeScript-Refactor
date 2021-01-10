b36bca2f1d0a2433edbc17e2933f67ea6dcd11fe
Tim Bakkum <timbakkum@gmail.com>
2021-01-10 19:37:31 +0100

    style: setup eslint and lint code

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,35 +1,33 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import {Item, GildedRose} from '../app/gilded-rose';
 
 const testItems = [
-    new Item("+5 Dexterity Vest", 10, 20), //
-    new Item("Aged Brie", 2, 0), //
-    new Item("Elixir of the Mongoose", 5, 7), //
-    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-    // this conjured item does not work properly yet
-    new Item("Conjured Mana Cake", 3, 6)];
+  new Item('+5 Dexterity Vest', 10, 20), //
+  new Item('Aged Brie', 2, 0), //
+  new Item('Elixir of the Mongoose', 5, 7), //
+  new Item('Sulfuras, Hand of Ragnaros', 0, 80), //
+  new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+  // this conjured item does not work properly yet
+  new Item('Conjured Mana Cake', 3, 6)];
 
-describe('Gilded Rose', function () {
+describe('Gilded Rose', function() {
+  it('should initialize items', function() {
+    const gildedRose = new GildedRose([new Item('+5 Dexterity Vest', 10, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual('+5 Dexterity Vest');
+  });
 
-    it('should initialize items', function() {
-        const gildedRose = new GildedRose([new Item("+5 Dexterity Vest", 10, 20)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).toEqual('+5 Dexterity Vest');
-    });
+  it('should update correctly', () => {
+    const gildedRose = new GildedRose(testItems);
+    const initialItems = gildedRose.currentItems;
 
-    it('should update correctly', () => {
-        const gildedRose = new GildedRose(testItems);
-        const initialItems = gildedRose.currentItems;
+    expect(initialItems[0].sellIn).toEqual(10);
+    expect(initialItems[0].quality).toEqual(20);
 
-        expect(initialItems[0].sellIn).toEqual(10);
-        expect(initialItems[0].quality).toEqual(20);
-
-        const items = gildedRose.updateQuality();
-        expect(items[0].sellIn).toEqual(9);
-        expect(items[0].quality).toEqual(19);
-    })
-
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(9);
+    expect(items[0].quality).toEqual(19);
+  });
 });
--- TypeScript/test/golden-master-text.spec.ts
+++ TypeScript/test/golden-master-text.spec.ts
@@ -1,34 +1,29 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import {Item, GildedRose} from '../app/gilded-rose';
 
-export const items = [
-    new Item("+5 Dexterity Vest", 10, 20), //
-    new Item("Aged Brie", 2, 0), //
-    new Item("Elixir of the Mongoose", 5, 7), //
-    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-    // this conjured item does not work properly yet
-    // new Item("Conjured Mana Cake", 3, 6)
+const items = [
+  new Item('+5 Dexterity Vest', 10, 20), //
+  new Item('Aged Brie', 2, 0), //
+  new Item('Elixir of the Mongoose', 5, 7), //
+  new Item('Sulfuras, Hand of Ragnaros', 0, 80), //
+  new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+  // this conjured item does not work properly yet
+  // new Item("Conjured Mana Cake", 3, 6)
 ];
-
 
 const gildedRose = new GildedRose(items);
 const days = 31;
 const daysArray = Array.from(Array(days), (_, i) => i);
 describe('golden master text test', () => {
-
-    test.each(daysArray)('day %i', day => {
-
-        let textSnapshot = "-------- day " + day + " -------- \n";
-        textSnapshot += "name, sellIn, quality \n";
-        items.forEach(element => {
-            textSnapshot += element.name + ' ' + element.sellIn + ' ' + element.quality+ '\n';
-
-        });
-        expect(textSnapshot).toMatchSnapshot();
-        gildedRose.updateQuality();
+  test.each(daysArray)('day %i', (day) => {
+    let textSnapshot = `-------- day ${day} -------- \n`;
+    textSnapshot += 'name, sellIn, quality \n';
+    items.forEach((element) => {
+      textSnapshot += `${element.name} ${element.sellIn} ${element.quality}\n`;
     });
-
+    expect(textSnapshot).toMatchSnapshot();
+    gildedRose.updateQuality();
+  });
 });
