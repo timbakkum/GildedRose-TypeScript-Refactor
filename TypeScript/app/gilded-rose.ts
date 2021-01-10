ebb80a4004805d1d72c9b22162f7b0044d139fe2
Tim Bakkum <timbakkum@gmail.com>
2021-01-10 21:01:38 +0100

    test: add unit tests for different item types, comments

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -30,13 +30,17 @@ export class GildedRose {
         if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (this.items[i].quality > 0) {
             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              // this block handles decreasing of quality of "regular" items
               this.items[i].quality = this.items[i].quality - 1;
             }
           }
         } else if (this.items[i].quality < 50) {
+          // this block handles increasing of quality
+          // of "brie" and "backstage passes"
           this.items[i].quality = this.items[i].quality + 1;
           // eslint-disable-next-line max-len
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            // this block handles "backstage passes" items
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -50,6 +54,8 @@ export class GildedRose {
           }
         }
         if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          // this block handles decreasing of sellIn of all items
+          // except legendary (decrease n.a.)
           this.items[i].sellIn = this.items[i].sellIn - 1;
         }
         if (this.items[i].sellIn < 0) {
@@ -58,14 +64,21 @@ export class GildedRose {
             if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
               if (this.items[i].quality > 0) {
                 if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                  // this block handles decreasing of quality
+                  // of normal items when sellIn hasn't dropped below 0
+
                   this.items[i].quality = this.items[i].quality - 1;
                 }
               }
             } else {
+              // this block handles sets "backstage passes" quality to 0
+              // after the concert has happened
               // eslint-disable-next-line max-len
               this.items[i].quality = this.items[i].quality - this.items[i].quality;
             }
           } else if (this.items[i].quality < 50) {
+            // this block handles the increasing of "brie"
+            // even when it's past it sell by date
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -19,15 +19,110 @@ describe('Gilded Rose', function() {
     expect(items[0].name).toEqual('+5 Dexterity Vest');
   });
 
-  it('should update correctly', () => {
-    const gildedRose = new GildedRose(testItems);
-    const initialItems = gildedRose.currentItems;
+  it('should handle updating regular items correctly', () => {
+    const regularItem = new Item('+5 Dexterity Vest', 2, 4);
+    const gildedRose = new GildedRose([regularItem]);
 
-    expect(initialItems[0].sellIn).toEqual(10);
-    expect(initialItems[0].quality).toEqual(20);
+    expect(regularItem.sellIn).toEqual(2);
+    expect(regularItem.quality).toEqual(4);
 
-    const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).toEqual(9);
-    expect(items[0].quality).toEqual(19);
+    gildedRose.updateQuality();
+    expect(regularItem.sellIn).toEqual(1);
+    expect(regularItem.quality).toEqual(3);
+
+    gildedRose.updateQuality();
+    expect(regularItem.sellIn).toEqual(0);
+    expect(regularItem.quality).toEqual(2);
+
+    gildedRose.updateQuality();
+    // now quality decreases by -2
+    expect(regularItem.sellIn).toEqual(-1);
+    expect(regularItem.quality).toEqual(0);
+
+    gildedRose.updateQuality();
+    // quality never goes below 0
+    expect(regularItem.sellIn).toEqual(-2);
+    expect(regularItem.quality).toEqual(0);
+  });
+
+  it('should handle updating "Aged Brie" correctly', () => {
+    const agedBrie = new Item('Aged Brie', 2, 0);
+    const gildedRose = new GildedRose([agedBrie]);
+
+    // quality increases normally when sellIn positive
+    expect(agedBrie.sellIn).toEqual(2);
+    expect(agedBrie.quality).toEqual(0);
+
+    gildedRose.updateQuality();
+    expect(agedBrie.sellIn).toEqual(1);
+    expect(agedBrie.quality).toEqual(1);
+
+    gildedRose.updateQuality();
+    expect(agedBrie.sellIn).toEqual(0);
+    expect(agedBrie.quality).toEqual(2);
+
+    gildedRose.updateQuality();
+    // quality now start to double
+    expect(agedBrie.sellIn).toEqual(-1);
+    expect(agedBrie.quality).toEqual(4);
+
+    for (let index = 0; index < 25; index++) {
+      gildedRose.updateQuality();
+    }
+    // quality never exceeds 50
+    expect(agedBrie.quality).not.toBeGreaterThan(50);
+  });
+
+  it('should handle updating backstage passes correctly', () => {
+    // eslint-disable-next-line max-len
+    const backstagePasses = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 25);
+    const gildedRose = new GildedRose([backstagePasses]);
+
+    gildedRose.updateQuality();
+    expect(backstagePasses.sellIn).toEqual(10);
+    expect(backstagePasses.quality).toEqual(26);
+
+    gildedRose.updateQuality();
+    // 10 days left to sell, so quality +2
+    expect(backstagePasses.sellIn).toEqual(9);
+    expect(backstagePasses.quality).toEqual(28);
+
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    // 5 days left to sell, so quality + 3
+    expect(backstagePasses.sellIn).toEqual(4);
+    expect(backstagePasses.quality).toEqual(39);
+
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    // last day to sell
+    expect(backstagePasses.sellIn).toEqual(0);
+    expect(backstagePasses.quality).toEqual(50);
+
+    gildedRose.updateQuality();
+    // concert has happened, pass is worthless
+    expect(backstagePasses.sellIn).toEqual(-1);
+    expect(backstagePasses.quality).toEqual(0);
+  });
+
+  it('handles updating "Sulfuras" correctly', () => {
+    const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+    const gildedRose = new GildedRose([sulfuras]);
+
+    expect(sulfuras.sellIn).toEqual(0);
+    expect(sulfuras.quality).toEqual(80);
+
+    gildedRose.updateQuality();
+    expect(sulfuras.sellIn).toEqual(0);
+    expect(sulfuras.quality).toEqual(80);
+
+    gildedRose.updateQuality();
+    expect(sulfuras.sellIn).toEqual(0);
+    expect(sulfuras.quality).toEqual(80);
   });
 });
